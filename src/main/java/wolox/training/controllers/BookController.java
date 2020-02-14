fe3c33b489d312c31a58e01e326dfe69b93d0d75
Ana Turrillo <anaturrillo@gmail.com>
2020-02-14 11:36:06 -0300

    Changes the name of the greet template to greeting, the correct name.

--- src/main/java/wolox/training/controllers/BookController.java
+++ src/main/java/wolox/training/controllers/BookController.java
@@ -10,6 +10,6 @@ public class BookController {
     @GetMapping("/greeting")
     public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
         model.addAttribute("name", name);
-        return "greet";
+        return "greeting";
     }
 }
