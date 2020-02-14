f10a869279bd44ebc70a535dce798c59634dce2a
Ana Turrillo <anaturrillo@gmail.com>
2020-02-14 17:55:20 -0300

    Creates repositories package and moves BooRepository into it. Adds find by author method to BookRepository.

--- src/main/java/wolox/training/repositories/BookRepository.java
+++ src/main/java/wolox/training/repositories/BookRepository.java
@@ -1,8 +1,8 @@
-package wolox.training.models;
+package wolox.training.repositories;
 
-import java.util.List;
 import org.springframework.data.repository.CrudRepository;
+import wolox.training.models.Book;
 
 public interface BookRepository extends CrudRepository<Book, Long> {
-    List<Book> findByTitle(String title);
+    Book findFirstByAuthor(String author);
 }
