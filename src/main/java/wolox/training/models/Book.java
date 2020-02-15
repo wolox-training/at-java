64062a19e13b9ccb599bbef7ac7904f882a106ef
Ana Turrillo <anaturrillo@gmail.com>
2020-02-15 15:19:56 -0300

    Undoes Lombok implementation due to errors. Will revist.

--- src/main/java/wolox/training/models/Book.java
+++ src/main/java/wolox/training/models/Book.java
@@ -11,53 +11,113 @@ import lombok.Setter;
 @Entity
 public class Book {
     @Id
-    @Getter
-    @Setter
     @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
 
-    @Getter
-    @Setter
     @Column()
     private String genre;
 
-    @Getter
-    @Setter
     @Column(nullable = false)
     private String author;
 
-    @Getter
-    @Setter
     @Column(nullable = false)
     private String image;
 
-    @Getter
-    @Setter
     @Column(nullable = false)
     private String title;
 
-    @Getter
-    @Setter
     @Column(nullable = false)
     private String subtitle;
 
-    @Getter
-    @Setter
     @Column(nullable = false)
     private String publisher;
 
-    @Getter
-    @Setter
     @Column(nullable = false)
     private String year;
 
-    @Getter
-    @Setter
     @Column(nullable = false)
     private int pages;
 
-    @Getter
-    @Setter
     @Column(nullable = false)
     private String isbn;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getSubtitle() {
+        return subtitle;
+    }
+
+    public void setSubtitle(String subtitle) {
+        this.subtitle = subtitle;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+
+    public int getPages() {
+        return pages;
+    }
+
+    public void setPages(int pages) {
+        this.pages = pages;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
 }
