1fd348645b6fa4516c74121f1128b8fd7622b8fa
Ana Turrillo <anaturrillo@gmail.com>
2020-02-15 16:52:02 -0300

    Removes unused import statement.

--- src/main/java/wolox/training/controllers/BookController.java
+++ src/main/java/wolox/training/controllers/BookController.java
@@ -1,7 +1,6 @@
 package wolox.training.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.DeleteMapping;
