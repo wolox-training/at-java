5a49454702558c976561f58a99a60ce8831c0dcb
Ana Turrillo <anaturrillo@gmail.com>
2020-02-21 18:48:49 -0300

    Improves BookAlreadyOwnedException. Creates ErrorConstats. Improves addBook method for User model.

--- src/main/java/wolox/training/exceptions/BookAlreadyOwnedException.java
+++ src/main/java/wolox/training/exceptions/BookAlreadyOwnedException.java
@@ -1,8 +1,8 @@
 package wolox.training.exceptions;
-import wolox.training.models.Book;
 
 public class BookAlreadyOwnedException extends RuntimeException {
-    public BookAlreadyOwnedException(Book book) {
-        super("The book " + book.getTitle() + " is already in the user's collection");
+
+    public BookAlreadyOwnedException(String bookTitle) {
+        super(String.format(ErrorConstats.BOOK_ALREADY_IN_COLLECTION, bookTitle));
     }
 }
---
+++ src/main/java/wolox/training/exceptions/ErrorConstats.java
@@ -0,0 +1,5 @@
+package wolox.training.exceptions;
+
+public class ErrorConstats {
+    public static final String BOOK_ALREADY_IN_COLLECTION = "The book %s is already in the user's collection";
+}
