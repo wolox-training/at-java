e7e5a9df95cc5b04f5a16870e941b1c92104612f
Ana Turrillo <anaturrillo@gmail.com>
2020-03-01 13:29:12 -0300

    For book controller, improves existing test and add new ones.

--- src/test/java/wolox/training/controllers/BookControllerSpec.java
+++ src/test/java/wolox/training/controllers/BookControllerSpec.java
@@ -1,25 +1,24 @@
 package wolox.training.controllers;
 
-import static org.hamcrest.Matchers.equalTo;
-import static org.mockito.BDDMockito.given;
-import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import java.util.Optional;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import wolox.training.mocks.MockBook;
+import wolox.training.mocks.InvalidBook;
 import wolox.training.models.Book;
 import wolox.training.repositories.BookRepository;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import wolox.training.utils.ResponseBodyMatchers;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(BookController.class)
@@ -31,28 +30,33 @@ public class BookControllerSpec {
     private BookRepository repository;
 
     @Test
-    public void onGet_givenAnAuthor_thenReturnsBook () throws Exception {
+    public void should_getBook_when_authorReceived () throws Exception {
         Book mockBook = MockBook.createOne();
-        given(repository.findFirstByAuthor("Dumas")).willReturn(mockBook);
+        Mockito.when(repository.findFirstByAuthor("Dumas")).thenReturn(Optional.of(mockBook));
 
         mvc.perform(MockMvcRequestBuilders.get("/api/books/Dumas")
                 .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
-                .andExpect(MockMvcResultMatchers.jsonPath("$.genre", equalTo(mockBook.getGenre())))
-                .andExpect(MockMvcResultMatchers.jsonPath("$.author", equalTo(mockBook.getAuthor())))
-                .andExpect(MockMvcResultMatchers.jsonPath("$.image", equalTo(mockBook.getImage())))
-                .andExpect(MockMvcResultMatchers.jsonPath("$.title", equalTo(mockBook.getTitle())))
-                .andExpect(MockMvcResultMatchers.jsonPath("$.subtitle", equalTo(mockBook.getSubtitle())))
-                .andExpect(MockMvcResultMatchers.jsonPath("$.publisher", equalTo(mockBook.getPublisher())))
-                .andExpect(MockMvcResultMatchers.jsonPath("$.year", equalTo(mockBook.getYear())))
-                .andExpect(MockMvcResultMatchers.jsonPath("$.pages", equalTo(mockBook.getPages())))
-                .andExpect(MockMvcResultMatchers.jsonPath("$.isbn", equalTo(mockBook.getIsbn())));
+                .andExpect(
+                        ResponseBodyMatchers
+                                .responseBody()
+                                .containsObjectAsJson(mockBook, Book.class)
+                );
     }
 
     @Test
-    public void onPost_givenABook_thenCreatesNewBook () throws Exception {
+    public void should_failToGetBook_when_authorBookDoesNotExist () throws Exception {
+        Mockito.when(repository.findFirstByAuthor("Dumas")).thenReturn(Optional.empty());
+
+        mvc.perform(MockMvcRequestBuilders.get("/api/books/Dumas")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    public void should_createBook_when_receivesBook () throws Exception {
         Book mockBook = MockBook.createOne();
-        when(repository.save(mockBook)).thenReturn(mockBook);
+        Mockito.when(repository.save(mockBook)).thenReturn(mockBook);
 
         ObjectMapper objectMapper = new ObjectMapper();
         String bookJSON = objectMapper.writeValueAsString(mockBook);
@@ -61,18 +65,55 @@ public class BookControllerSpec {
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(bookJSON))
                 .andExpect(status().isCreated())
-                .andDo(mvcResult -> {
-                    String pepe = mvcResult.getResponse().getContentAsString();
-                    assert(mvcResult).equals(mockBook);
-                });
-                /*.andExpect(MockMvcResultMatchers.jsonPath("$.genre", equalTo(mockBook.getGenre())))
-                .andExpect(MockMvcResultMatchers.jsonPath("$.author", equalTo(mockBook.getAuthor())))
-                .andExpect(MockMvcResultMatchers.jsonPath("$.image", equalTo(mockBook.getImage())))
-                .andExpect(MockMvcResultMatchers.jsonPath("$.title", equalTo(mockBook.getTitle())))
-                .andExpect(MockMvcResultMatchers.jsonPath("$.subtitle", equalTo(mockBook.getSubtitle())))
-                .andExpect(MockMvcResultMatchers.jsonPath("$.publisher", equalTo(mockBook.getPublisher())))
-                .andExpect(MockMvcResultMatchers.jsonPath("$.year", equalTo(mockBook.getYear())))
-                .andExpect(MockMvcResultMatchers.jsonPath("$.pages", equalTo(mockBook.getPages())))
-                .andExpect(MockMvcResultMatchers.jsonPath("$.isbn", equalTo(mockBook.getIsbn())));*/
+                .andExpect(
+                        ResponseBodyMatchers
+                                .responseBody()
+                                .containsObjectAsJson(mockBook, Book.class)
+                );
+    }
+
+    @Test
+    public void should_failToCreateBook_when_bookHasNoAuthor () throws Exception {
+        InvalidBook invalidBook = new InvalidBook();
+        InvalidBook noAuthorBook = invalidBook.bookWithoutAuthor();
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        String bookJSON = objectMapper.writeValueAsString(noAuthorBook);
+
+        mvc.perform(MockMvcRequestBuilders.post("/api/books")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(bookJSON))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test void should_updateBook_when_recievesBookToUpdate () throws Exception {
+        Book mockBook = MockBook.createOne();
+        Mockito.when(repository.existsById(mockBook.getId())).thenReturn(true);
+        Mockito.when(repository.save(mockBook)).thenReturn(mockBook);
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        String bookJSON = objectMapper.writeValueAsString(mockBook);
+
+        mvc.perform(MockMvcRequestBuilders.put("/api/books/" + mockBook.getId())
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(bookJSON))
+                .andExpect(status().isOk())
+                .andExpect(
+                        ResponseBodyMatchers
+                                .responseBody()
+                                .containsObjectAsJson(mockBook, Book.class)
+                );
+    }
+
+    @Test void should_failToUpdateBook_when_bookIdDoesNotMatchRequestedId () throws Exception {
+        Book mockBook = MockBook.createOne();
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        String bookJSON = objectMapper.writeValueAsString(mockBook);
+
+        mvc.perform(MockMvcRequestBuilders.put("/api/books/1")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(bookJSON))
+                .andExpect(status().isBadRequest());
     }
 }
