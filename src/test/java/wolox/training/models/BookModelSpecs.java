b2e66b45e198a7e204d6f6b84bd53545aba32331
Ana Turrillo <anaturrillo@gmail.com>
2020-03-02 10:43:38 -0300

    Improves BookModelSpecs

--- src/test/java/wolox/training/models/BookModelSpecs.java
+++ src/test/java/wolox/training/models/BookModelSpecs.java
@@ -2,21 +2,15 @@ package wolox.training.models;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import java.util.List;
 import java.util.Optional;
-import javax.persistence.criteria.CriteriaBuilder.In;
-import javax.transaction.Transactional;
 import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.transaction.annotation.Propagation;
-import wolox.training.mocks.InvalidBook;
 import wolox.training.mocks.MockBook;
 import wolox.training.repositories.BookRepository;
 
@@ -28,7 +22,6 @@ public class BookModelSpecs {
 
     @Autowired
     private BookRepository repository;
-
     @Test
     void should_updateBook_when_recievesBookToUpdate () throws Exception {
         Book mockBook = MockBook.createOne();
@@ -46,8 +39,11 @@ public class BookModelSpecs {
         try {
             repository.save(book);
             repository.flush();
-        } catch (Exception ex) {
-            assertThat(repository.findById(book.getId())).isNotPresent();
+        } catch (DataIntegrityViolationException ex) {
+            return;
         }
+
+        Assert.fail();
     }
 }
+
