057d4938e2030783bf25ade6fbcba216567b39fe
Ana Turrillo <anaturrillo@gmail.com>
2020-03-09 17:49:23 -0300

    Disables csrf in SecurityConfig.

--- src/main/java/wolox/training/config/SecurityConfig.java
+++ src/main/java/wolox/training/config/SecurityConfig.java
@@ -26,9 +26,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-                .anyRequest().authenticated()
-                .and().httpBasic();
+        http.csrf().disable().httpBasic()
+                .and()
+                .authorizeRequests()
+                .anyRequest().authenticated();
     }
 
     @Override
